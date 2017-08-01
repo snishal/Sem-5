f7d251a7fa03a1ff85f100720e72f4d505978dba
snishal <snishal33@gmail.com>
2017-08-02 00:25:09 +0530

    q3

--- SysProg/lex.yy.c
+++ SysProg/lex.yy.c
@@ -375,8 +375,8 @@ static void yy_fatal_error (yyconst char msg[]  );
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
 
-#define YY_NUM_RULES 2
-#define YY_END_OF_BUFFER 3
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -384,9 +384,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[7] =
+static yyconst flex_int16_t yy_accept[8] =
     {   0,
-        0,    0,    3,    2,    1,    0
+        0,    0,    4,    2,    1,    1,    0
     } ;
 
 static yyconst YY_CHAR yy_ec[256] =
@@ -423,27 +423,27 @@ static yyconst YY_CHAR yy_ec[256] =
 
 static yyconst YY_CHAR yy_meta[3] =
     {   0,
-        1,    1
+        1,    2
     } ;
 
-static yyconst flex_uint16_t yy_base[7] =
+static yyconst flex_uint16_t yy_base[9] =
     {   0,
-        0,    0,    3,    4,    4,    4
+        0,    0,    4,    5,    0,    0,    5,    1
     } ;
 
-static yyconst flex_int16_t yy_def[7] =
+static yyconst flex_int16_t yy_def[9] =
     {   0,
-        6,    1,    6,    6,    6,    0
+        7,    1,    7,    7,    8,    8,    0,    7
     } ;
 
-static yyconst flex_uint16_t yy_nxt[7] =
+static yyconst flex_uint16_t yy_nxt[8] =
     {   0,
-        4,    5,    6,    3,    6,    6
+        4,    5,    6,    7,    3,    7,    7
     } ;
 
-static yyconst flex_int16_t yy_chk[7] =
+static yyconst flex_int16_t yy_chk[8] =
     {   0,
-        1,    1,    3,    6,    6,    6
+        1,    1,    8,    3,    7,    7,    7
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -460,9 +460,11 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "scissor_cipher.l"
-#line 2 "scissor_cipher.l"
-#line 466 "lex.yy.c"
+#line 1 "largestword.l"
+#line 2 "largestword.l"
+	int length = 0;
+	char *word;
+#line 468 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -680,10 +682,10 @@ YY_DECL
 		}
 
 	{
-#line 4 "scissor_cipher.l"
+#line 6 "largestword.l"
 
 
-#line 687 "lex.yy.c"
+#line 689 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -710,13 +712,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 7 )
+				if ( yy_current_state >= 8 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 4 );
+		while ( yy_base[yy_current_state] != 5 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -742,20 +744,18 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "scissor_cipher.l"
-{ 
-			*yytext += 3;
-			if(*yytext > 90 && *yytext < 97){
-				*yytext = *yytext - 91 + 65; 
-			}
-			else if(*yytext > 122){
-				*yytext = *yytext - 123 + 97;
-			}
-			printf("%s", yytext);}
+#line 8 "largestword.l"
+{ if(yyleng > length){length = yyleng; word = yytext; }}
 	YY_BREAK
 case 2:
+/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 16 "scissor_cipher.l"
+#line 9 "largestword.l"
+{ }
+	YY_BREAK
+case 3:
+YY_RULE_SETUP
+#line 11 "largestword.l"
 ECHO;
 	YY_BREAK
 #line 762 "lex.yy.c"
@@ -1051,7 +1051,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 7 )
+			if ( yy_current_state >= 8 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1079,11 +1079,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 7 )
+		if ( yy_current_state >= 8 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 6);
+	yy_is_jam = (yy_current_state == 7);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1759,7 +1759,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 16 "scissor_cipher.l"
+#line 11 "largestword.l"
 
 
 
@@ -1767,6 +1767,13 @@ int main(){
 
 	yyin = fopen("input.txt", "r");
 	yylex();
+	printf("Length : %d \nWord : ", length);
+	for(int i=0; i < length; i++){
+		char c = *word;
+		printf("%c", c);
+		word = word + 1;
+	}
+	printf("\n");
 	return 0;
 
 }
